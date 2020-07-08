1b22019cd1b2ba1aeb9af04ad15bb72c1356699a
Anna Romashenko <aa.romashchenko@gmail.com>
2020-07-08 16:20:15 +0300

    Updates

---
+++ precache-manifest.cee4e9bba391c2ee9f779fe1fa65549d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5679fd84360d9e9d77a078e3f01bfbe",
+    "url": "/nails-poppy/index.html"
+  },
+  {
+    "revision": "dacefd9dcc83f31ea358",
+    "url": "/nails-poppy/static/css/main.835b3cf4.chunk.css"
+  },
+  {
+    "revision": "8771bb47e5fb75f4e900",
+    "url": "/nails-poppy/static/js/2.77fb3012.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/nails-poppy/static/js/2.77fb3012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dacefd9dcc83f31ea358",
+    "url": "/nails-poppy/static/js/main.1d82757b.chunk.js"
+  },
+  {
+    "revision": "912f1aa6a1e174831247",
+    "url": "/nails-poppy/static/js/runtime-main.61a256c8.js"
+  },
+  {
+    "revision": "7c3daf12b706645b5d3710f863a4da04",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-300.7c3daf12.woff2"
+  },
+  {
+    "revision": "8dc95fab9cf98d02ca8d76e97d3dff60",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-300.8dc95fab.woff"
+  },
+  {
+    "revision": "8b763220218ffc11c57c84ddb80e7b26",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-500.8b763220.woff"
+  },
+  {
+    "revision": "92d16e458625f4d2c8940f6bdca0ff09",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-500.92d16e45.woff2"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "39d93cf678c740f9f6b2b1cfde34bee3",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-700.39d93cf6.woff2"
+  },
+  {
+    "revision": "80f10bd382f0df1cd650fec59f3c9394",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-700.80f10bd3.woff"
+  },
+  {
+    "revision": "9a9befcf50d64f9d2d19d8b1d1984add",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-800.9a9befcf.woff"
+  },
+  {
+    "revision": "b7018be9ed6cd94da8b6675b3a468c3b",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-800.b7018be9.woff2"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/nails-poppy/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  }
+]);
